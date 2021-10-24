9ebfc44cbb288bfa2e0386af2bfbb7c79f8f3bfd
yir-win <yangjiriver@163.com>
2021-10-24 16:19:54 +0800

    fix some bug

--- 动态口令验证/Token_generation.py
+++ 动态口令验证/Token_generation.py
@@ -1,7 +1,6 @@
-import time  
+import time
 import hashlib
 
-# 这里还用bug 可能出现5位数或更少位数
 
 # 获取时间
 current_time = time.time()
@@ -12,5 +11,5 @@ seed = "19300240012"
 # hash运算
 md5.update((seed + str(int(current_time/60))).encode('utf-8'))
 hashAns = md5.hexdigest()
-# 将运算结果保留装换为整数 取末尾6个数字
-print(int(hashAns, 16) % 1000000)
+# 将运算结果保留装换为整数 取末尾6个数字 
+print((str(int(hashAns, 16) % 1000000)).zfill(6))
--- 动态口令验证/client.py
+++ 动态口令验证/client.py
@@ -6,33 +6,34 @@ clientSocket = socket(AF_INET, SOCK_STREAM) # 建立TCP套接字，使用IPv4协
 clientSocket.connect((serverName,serverPort)) # 向服务器发起连接
 sha256 = hashlib.sha256()
 
-id = input('输入用户名：').encode() # 用户输入信息，并编码为bytes以便发送
+id = input('输入用户名：\n').encode() # 用户输入信息，并编码为bytes以便发送
 print("用户名为：",id.decode())
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~下面未完成
-clientSocket.send(id) # 将信息发送到服务器
-print("发送id成功")
-feedback = clientSocket.recvfrom(1024) # 从服务器接收信息
+ # 将信息发送到服务器
+clientSocket.send(id)
+# 从服务器接收信息
+feedback = clientSocket.recvfrom(1024) 
+# 打印服务器的反馈
 print(feedback[0].decode())
 
 if feedback[0] == "Start certification".encode():
     # 如果获得服务器正常的响应
-    PIN = input('请输入PIN')
+    PIN = input('请输入PIN：\n')
+    
     # 先本地对PIN进行hash
     PIN = PIN.encode('utf-8')
     sha256_forPIN = hashlib.sha256()
     sha256_forPIN.update(PIN)
     PIN = sha256_forPIN.hexdigest()
-    print(PIN)
 
-
-    seed = input('请输入令牌上显示的数组')
+    # 对时间令牌和PIN的hash结果合并后再hash
+    seed = input('请输入令牌上显示的数组:\n')
     identify_value = (PIN + str(seed)).encode('utf-8')
     sha256.update(identify_value)
     identify_value = sha256.hexdigest()
     clientSocket.send(identify_value.encode())
-    print("发送验证：",identify_value)
 
+# 打印服务器的反馈
 feedback2 = clientSocket.recvfrom(1024)
 print(feedback2[0].decode())
 
